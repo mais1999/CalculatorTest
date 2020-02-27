a14ee8ebaed6286d6ce461ccfbd66625ca091300
Sewar <Sewar@DESKTOP-QQU258B>
2020-02-27 09:46:11 +0200

    first commit

---
+++ src/calculatorSoft/CalcText.java
@@ -0,0 +1,5 @@
+package calculatorSoft;
+
+public class CalcText {
+
+}
---
+++ src/calculatorSoft/CalcTextTest.java
@@ -0,0 +1,14 @@
+package calculatorSoft;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcTextTest {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
